949da274e79f4d66092e962b96be40a7777fd688
song geng <827265717@qq.com>
2020-02-04 21:01:05 -0500

    Update test_weather.py

--- test_weather.py
+++ test_weather.py
@@ -1,9 +1,10 @@
 from weather import weather_report
 
-# This are valid inputs
-weather_report('beijing')
-weather_report('boston')
-weather_report('london')
-# This are invalid inputs
-weather_report('osama')
-weather_report('songgeng')
+def test():
+    # This are valid inputs
+    weather_report('beijing')
+    weather_report('boston')
+    weather_report('london')
+    # This are invalid inputs
+    weather_report('osama')
+    weather_report('songgeng')
